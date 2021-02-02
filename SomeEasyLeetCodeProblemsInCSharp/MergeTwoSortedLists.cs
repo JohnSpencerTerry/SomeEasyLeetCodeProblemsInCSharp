fa1ede4e05e5f0a02ecbd486bc3f83db43a2f9b8
John.Spencer.Terry <John.Spencer.Terry@gmail.com>
2021-02-02 09:02:29 -0500

    added merge exercise

---
+++ SomeEasyLeetCodeProblemsInCSharp/MergeTwoSortedLists.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static SomeEasyLeetCodeProblemsInCSharp.Soution;
+
+namespace SomeEasyLeetCodeProblemsInCSharp
+{
+    public partial class Soution      
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+
+    public partial class SolutionTest
+    {
+        public void TestMergeTwoLists(ListNode l1, ListNode l2, ListNode expected)
+        {
+
+        }
+    }
+
+
+}
